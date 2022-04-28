1e6573d30f7867a07b70f48b4fd59b2a3cd8b107
Daekeun Ko <kdkcom1234@gmail.com>
2022-04-28 13:07:49 +0900

    띠 애니메이션 변경

--- js/planet.js
+++ js/planet.js
@@ -5,68 +5,6 @@
   const img = new Image();
   const text = document.querySelector('#text-pmr');
 
-  function createKeyframes() {
-    let frames = '';
-    const framesCount = 360;
-    for (let i = 0; i < framesCount; i++) {
-      const quarter = Math.floor(framesCount / 4);
-      const quaterNum = Math.floor(i / quarter);
-      const frameName = ((i / framesCount) * 100).toFixed(2);
-
-      const cur = (1 * ((quarter - (i % quarter)) / quarter)).toFixed(2);
-      const next = (1 * ((i % quarter) / quarter)).toFixed(2);
-
-      switch (quaterNum) {
-        case 0:
-          frames += `${frameName}% {
-          border-top: 5px solid rgba(0, 255, 255, ${cur});
-          border-left: 5px solid rgba(0, 255, 255, ${next});
-          border-bottom: 5px solid rgba(0, 255, 255, 0);
-          border-right: 5px solid rgba(0, 255, 255, 0);
-        } `;
-          break;
-        case 1:
-          frames += `${frameName}% {
-          border-top: 5px solid rgba(0, 255, 255, 0);
-          border-left: 5px solid rgba(0, 255, 255, ${cur});
-          border-bottom: 5px solid rgba(0, 255, 255, ${next});
-          border-right: 5px solid rgba(0, 255, 255, 0);
-        } `;
-          break;
-        case 2:
-          frames += `${frameName}% {
-          border-top: 5px solid rgba(0, 255, 255, 0);
-          border-left: 5px solid rgba(0, 255, 255, 0);
-          border-bottom: 5px solid rgba(0, 255, 255, ${cur});
-          border-right: 5px solid rgba(0, 255, 255, ${next});               
-        } `;
-          break;
-        case 3:
-          frames += `${frameName}% {
-          border-top: 5px solid rgba(0, 255, 255, ${next});   
-          border-left: 5px solid rgba(0, 255, 255, 0);   
-          border-bottom: 5px solid rgba(0, 255, 255, 0);
-          border-right: 5px solid rgba(0, 255, 255, ${cur});
-        } `;
-          break;
-      }
-    }
-
-    frames += `100% {
-      border-top: 5px solid rgba(0, 255, 255, 1);
-      border-left: 5px solid rgba(0, 255, 255, 0);    
-      border-bottom: 5px solid rgba(0, 255, 255, 0);
-      border-right: 5px solid rgba(0, 255, 255, 0);           
-    }`;
-
-    const keyframes = `@keyframes spin {
-      ${frames}
-    }`;
-
-    const style = `<style>${keyframes}</style>`;
-    document.head.insertAdjacentHTML('beforeend', style);
-  }
-
   function play(entries) {
     entries[0].isIntersecting &&
       requestAnimationFrame(() => {
@@ -99,8 +37,6 @@
         text.style.animation = 'text-fade-in 1s ease-in-out forwards';
       });
   }
-
-  createKeyframes();
 
   const observer = new IntersectionObserver(play, { threshold: 0.3 });
   observer.observe(container);
