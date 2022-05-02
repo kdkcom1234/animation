355440dcf6301136ea08d5781e13a7b477188b1d
Daekeun Ko <kdkcom1234@gmail.com>
2022-05-02 16:49:17 +0900

    timeline 애니메이션 스타일 조정

--- js/timeline.js
+++ js/timeline.js
@@ -3,10 +3,8 @@
 
   function move(entries) {
     if (entries[0].isIntersecting) {
-      // console.log(entries[0].target, entries[0].target.className, entries[0].isIntersecting);
       const container = entries[0].target;
-      container.style.opacity = 1;
-      container.style.transform = 'translateX(0px)';
+      container.classList.add('fade-in');
     }
   }
 
