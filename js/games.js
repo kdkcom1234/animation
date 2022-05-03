4bce389b07b42873b1dd3f00f5a5934363a25bdc
Daekeun Ko <kdkcom1234@gmail.com>
2022-05-03 16:39:04 +0900

    zoom-out 복원

--- js/games.js
+++ js/games.js
@@ -2,8 +2,8 @@
   const top = document.querySelector('#top-intro-wrapper');
   const features = document.querySelector('#features-wrapper');
   const games = document.querySelector('#games');
-  // const gamesPhoneWrapper = document.querySelector('#games-phone-wrapper ');
-  // const phoneHeight = 310;
+  const gamesPhoneWrapper = document.querySelector('#games-phone-wrapper ');
+  const phoneHeight = 310;
 
   function fadeOutAndZoomOut() {
     const topHeight = top.getBoundingClientRect().height;
@@ -11,8 +11,8 @@
     const gamesHeight = games.getBoundingClientRect().height;
 
     const offset = scrollY - (topHeight + featuresHeight);
-    const ratio = offset / gamesHeight;
-    // const ratio = (offset - gamesHeight / 2) / (gamesHeight / 2);
+    // const ratio = offset / gamesHeight;
+    const ratio = (offset - gamesHeight / 2) / (gamesHeight / 2);
 
     //fade - out;
     if (ratio >= 0 && ratio <= 1) {
@@ -21,15 +21,15 @@
       games.style.visibilisty = 'hidden';
     }
 
-    // // zoom-out
-    // const scaleRatio = 1 - (ratio + 1);
-    // const scaleFactor = window.screen.height / window.devicePixelRatio / phoneHeight;
+    // zoom-out
+    const scaleRatio = 1 - (ratio + 1);
+    const scaleFactor = window.screen.height / window.devicePixelRatio / phoneHeight;
 
-    // if (scaleRatio >= 1 / scaleFactor && scaleRatio <= 1) {
-    //   gamesPhoneWrapper.style.transform = `scale(${scaleRatio * scaleFactor})`;
-    // } else if (scaleRatio > 1) {
-    //   gamesPhoneWrapper.style.transform = `scale(${scaleFactor})`;
-    // }
+    if (scaleRatio >= 1 / scaleFactor && scaleRatio <= 1) {
+      gamesPhoneWrapper.style.transform = `scale(${scaleRatio * scaleFactor})`;
+    } else if (scaleRatio > 1) {
+      gamesPhoneWrapper.style.transform = `scale(${scaleFactor})`;
+    }
   }
 
   function play(entries) {
